731e898e4723ba27a6d27b3ae2f13152c9c45e33
wota328 <w.ota@axelentermedia.jp>
2023-05-04 16:31:54 +0900

    sec03_itemsgadekinai

--- resources/js/Layouts/Pages/Items/Index.vue
+++ resources/js/Layouts/Pages/Items/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
-import FlashMessage from '@/Components/FlashMessage.vue'
+//import FlashMessage from '@/Components/FlashMessage.vue'
 
 defineProps({
   items: Array
@@ -24,7 +24,7 @@ defineProps({
                     <div class="p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font">
                           <div class="container px-5 py-8 mx-auto">
-                            <FlashMessage />
+                           <!-- <FlashMessage /> -->
                             <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
                               <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
                             </div>
